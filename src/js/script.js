52bdde606c3e0bd54fe98c1130cf0097c2e49337
Yufenyuy Veyeh Didier <yveyeh@gmail.com>
2021-12-09 16:09:21 +0100

    Display feedback

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function ($) {
+
     $('#upload_file').on('click', function () {
         $('[name="upload_file"]').click()
         $('[name="upload_file"]').on('change', function () {
@@ -16,12 +17,53 @@ $(document).ready(function ($) {
                 method: 'POST',
                 success: function (res_data) {
                     console.log(res_data)
-                    
+                    display_feedback(res_data.message, res_data.type) // display feedback
                 },
                 error: function (res_data) {
                     console.log(res_data)
+                    display_feedback(res_data.message, res_data.type) // display feedback
                 }
             })
         })
     })
+
+    /**
+     * Display feedback message in html.
+     * @param {string} message The feedback message.
+     * @param {string} type The feedback type `(error|success)`.
+     */
+    var display_feedback = (message, type) => {
+        show_feedback(message, type)
+        setTimeout(() => { // hide feedback after showing it
+            hide_feedback(type)
+        }, 3000)
+    }
+
+    /**
+     * show feedback message in html.
+     * @param {string} message The feedback message.
+     * @param {string} type The feedback type `(error|success)`.
+     */
+    var show_feedback = (message, type) => {
+        // show the feedback container
+        $('#feedback').css('display', 'block')
+        // apply styling for the corresponding feedback
+        type == 'error' ? $('#feedback span.alert').addClass('alert-error') : $('#feedback span.alert').addClass('alert-success')
+        // inject feedback message
+        $('#feedback span').html(message)
+    }
+
+    /**
+     * Hides the feedback in html.
+     * @param {string} type The feedback type `(error|success)`.
+     */
+    var hide_feedback = (type) => {
+        // hide the feedback container
+        $('#feedback').css('display', 'none')
+        // apply styling for the corresponding feedback
+        type == 'error' ? $('#feedback span.alert').removeClass('alert-error') : $('#feedback span.alert').removeClass('alert-success')
+        // remove feedback message
+        $('#feedback span').html('')
+    }
+
 })
